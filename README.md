517717623c2389055a1fdc0ad0e9f0356baadd55
James Njuguna <james@arifu.com>
2021-03-03 06:14:26 +0300

    --my first commit for the python is easy course with the first module variables

---
+++ README.md
@@ -0,0 +1,5 @@
+# python_is_easy_course by pirple.com
+
+Module 1
+
+Variables
---
+++ variables/main.py
@@ -0,0 +1,70 @@
+"""
+Homework Assignment #1: Variables
+
+Create variables for song attributes and print them:
+E.g
+
+Artist = "Dave Brubeck"
+Genre = "Jazz"
+DurationInSeconds = 328
+
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+"""
+
+"""
+JAZZ Genre
+"""
+print("My favorite Jazz songs are: ")
+Artist = "Miles Davis"
+name = "So What"
+Genre = "Jazz"
+Album = "Kind of Blue"
+Year = 1958
+DurationInSeconds = 328
+
+print(f'Name of Artist: {Artist}')
+print(f'Name of Song: {name}')
+print(f'Genre: {Genre}')
+print(f'Album: {Album}')
+print(f'Year: {Year}')
+print(f'DurationInSeconds: {DurationInSeconds}')
+
+"""
+R & B Genre
+"""
+print("My favorite R & B songs are: ")
+Artist = "Usher"
+name = "My Way"
+Genre = "R & B"
+Album = "My Way"
+Year = 1997
+DurationInSeconds = 428
+
+print(f'Name of Artist: {Artist}')
+print(f'Name of Song: {name}')
+print(f'Genre: {Genre}')
+print(f'Album: {Album}')
+print(f'Year: {Year}')
+print(f'DurationInSeconds: {DurationInSeconds}')
+
+
+"""
+Reggea Genre
+"""
+print("My favorite Reggea songs are: ")
+Artist = "Busy Signal"
+name = "Girl Like You"
+Genre = "Reggea"
+Album = "Parts Of The Puzzle"
+Year = 2019
+DurationInSeconds = 350
+
+print(f'Name of Artist: {Artist}')
+print(f'Name of Song: {name}')
+print(f'Genre: {Genre}')
+print(f'Album: {Album}')
+print(f'Year: {Year}')
+print(f'DurationInSeconds: {DurationInSeconds}')
+
